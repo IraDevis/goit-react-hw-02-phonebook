28b9650b688089c1fbb7cf7c25340a81c4fbc76b
Iryna Volosozharova <iradevis@gmail.com>
2021-07-24 20:09:26 +0300

    Updates

--- static/js/main.8cbdad6a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{12:function(t,e,n){},14:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n.n(o),r=n(3),i=n.n(r),a=n(4),s=n(6),h=n(5),p=(n(11),n(12),function(t){Object(s.a)(n,t);var e=Object(h.a)(n);function n(){return Object(a.a)(this,n),e.apply(this,arguments)}return n}(o.Component)),b=n(2);i.a.render(Object(b.jsx)(c.a.StrictMode,{children:Object(b.jsx)(p,{})}),document.getElementById("root"))}},[[14,1,2]]]);
-//# sourceMappingURL=main.8cbdad6a.chunk.js.map
